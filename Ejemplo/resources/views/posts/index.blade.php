210ea2069dc49c38158b862cd156f94e93e25621
ricardcutzh <ricardcutzh@gmail.com>
2017-11-23 22:08:24 -0600

    Primer ejemplo de Uso en Laravel 2

---
+++ Ejemplo/resources/views/posts/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.app')
+@section('content')
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Title</h2>
+      <p>Posted 5 Hours Ago</p>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Title</h2>
+      <p>Posted 5 Hours Ago</p>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Title</h2>
+      <p>Posted 5 Hours Ago</p>
+    </div>
+  </div>
+
+  <hr>
+@endsection
