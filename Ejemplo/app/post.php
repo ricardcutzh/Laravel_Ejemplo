ec4ef2947a4482bdf42b423cdaa245c45d4e7c7e
ricardcutzh <ricardcutzh@gmail.com>
2017-11-29 23:27:58 -0600

    creacion de migracion en bd

--- Ejemplo/app/post.php
+++ Ejemplo/app/post.php
@@ -9,6 +9,8 @@ class Post extends Model{
     protected $table = 'posts';//ESPECIFICAR A QUE TABLA DE LA MIGRACION ME REFIERO
 
     protected $fillable = ['title','description','url'];//ATRIBUTOS QUE SE PUEDEN LLENAR EN LA TABLA CREADA
+
+    //EXISTE TAMBIEN UN ATRIBUTO GUARDED ESPECIFICA LAS COLUMNAS QUE NO NOS INTERESA QUE SEAN LLENADA
 }
 
 
